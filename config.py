99cbbd38a96430b7f88120527e74a84881b98ef5
Jacob <61052022+Airiuwu@users.noreply.github.com>
2021-10-21 02:17:38 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 host = '' # Host e.g. (localhost, 127.0.0.1)
 port = '' # Port which you would like to run the server on
 secretKey = '' # https://www.lastpass.com/features/password-generator
-screenshotPath = '' # /home/Airi/screenshots/
+screenshotPath = '' # /home/Airi/ShareX-Uploader/screenshots/
 mainURL = '' # https://google.com/screenshots/
