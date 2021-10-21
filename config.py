4833ec74401dd4444ea5e0b390d21ca768e134e2
Jacob <61052022+Airiuwu@users.noreply.github.com>
2021-10-21 02:16:57 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+host = '' # Host e.g. (localhost, 127.0.0.1)
+port = '' # Port which you would like to run the server on
+secretKey = '' # https://www.lastpass.com/features/password-generator
+screenshotPath = '' # /home/Airi/screenshots/
+mainURL = '' # https://google.com/screenshots/
