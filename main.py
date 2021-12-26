29cd854c6355c2967f5b14fda16f590c047a8ec2
Jacob <61052022+Airiuwu@users.noreply.github.com>
2021-12-25 21:51:00 -0500

    Add support for multiple files

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import config
-from quart import Quart, send_file, request
-from os import path
-from random import choice
 from string import ascii_uppercase, digits, ascii_lowercase
+from quart import Quart, send_file, request
+from random import choice
+import config, imghdr
+from os import path
 
 app = Quart(__name__)
 
@@ -11,30 +11,37 @@ async def randomString(size=8, chars=ascii_uppercase + digits + ascii_lowercase)
 
 @app.route("/screenshots/upload", methods=["POST"])
 async def uploadScreenshot():
-    u = request.args.get("u")
-    request_files = await request.files
-    ssFile = request_files["sh"]
-    ssName = await randomString()
+	u = request.args.get("u")
+	request_files = await request.files
+	ss_file = request_files["sh"]
+	ss_name = await randomString()
+	ss_data = ss_file.read()
 
-    if request.args.get("key") != config.secretKey:
-        return f"Hello, {u} it would seem that your secret key is incorrect."
+	if request.args.get("key") != config.secretKey:
+		return f"Hello, {u} it would seem that your secret key is incorrect."
 
-    if not ssFile:
-        return "I see what you're doing here."
-    
-    if path.exists(f"{config.screenshotPath}{ssName}.png"):    
-        ssName = await randomString()
+	if not ss_file:
+		return "I see what you're doing here."
 
-    with open(f"{config.screenshotPath}{ssName}.png", "wb") as file:
-        file.write(ssFile.read())
+	if not (ext := imghdr.what(h=ss_data, file="")) in ("gif", "png", "jpeg"):
+		return "Unknown extension."
 
-    return f"{config.mainURL}{ssName}"
+	screenshot_file = f"{config.screenshotPath}{ss_name}.{ext}"
+
+	if path.exists(screenshot_file):
+		ss_name = await randomString()
+
+	with open(screenshot_file, "wb") as file:
+		file.write(ss_data)
+
+	return f"{config.mainURL}{ss_name}.{ext}"
 
 @app.route("/screenshots/<string:screenshotID>")
 async def getScreenshot(screenshotID):
-    if path.exists(f"{config.screenshotPath}{screenshotID}.png"):    
-        return await send_file(f"{config.screenshotPath}{screenshotID}.png")
-    else:
-        return "We could not find the screenshot you were looking for."
+	_path = f"{config.screenshotPath}{screenshotID}"
+	if path.exists(_path): 
+		return await send_file(_path)
+
+	return "We could not find the screenshot you were looking for."
 
 app.run(host=config.host, port=config.port)
