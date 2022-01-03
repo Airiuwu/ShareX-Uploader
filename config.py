9afc1715d2f6bc99b0ab113859299ba30810356a
Jacob <61052022+Airiuwu@users.noreply.github.com>
2022-01-03 02:59:05 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 host = '' # Host e.g. (localhost, 127.0.0.1)
 port = '' # Port which you would like to run the server on
-secretKey = '' # https://www.lastpass.com/features/password-generator
+secretKey = '' # Execute utils/passGen.py
 screenshotPath = '' # /home/Airi/ShareX-Uploader/screenshots/
 mainURL = '' # https://google.com/screenshots/
 allowedExtensions = ['gif', 'png', 'jpeg', 'jpg'] # Can be any file extension really, these are just left for general use
