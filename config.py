5f5af71f529290cf09316afc1451fb8c849c9627
Jacob <61052022+Airiuwu@users.noreply.github.com>
2022-01-03 03:11:26 -0500

    hot

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 host = '' # Host e.g. (localhost, 127.0.0.1)
 port = '' # Port which you would like to run the server on
+mainURL = '' # https://google.com/screenshots/
+uploadURL = '' # mainURL with /upload added at the end
 secretKey = '' # Execute utils/passGen.py
 screenshotPath = '' # /home/Airi/ShareX-Uploader/screenshots/
-mainURL = '' # https://google.com/screenshots/
 allowedExtensions = ['gif', 'png', 'jpeg', 'jpg'] # Can be any file extension really, these are just left for general use
